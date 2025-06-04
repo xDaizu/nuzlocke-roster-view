4015954136dd8afa8b225c4d3875006238f0cbe8
Danny Even <danielparejom@gmail.com>
2025-06-04 19:14:57 +0200

    Modified basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/nuzlocke-roster-view">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/public" element={<PublicView />} />
