e175232d877c1e4cec5f12f6baab2d72b4a68f30
Danny Even <danielparejom@gmail.com>
2025-06-05 20:27:54 +0200

    add atreyu

--- src/data/fixtures.ts
+++ src/data/fixtures.ts
@@ -4,5 +4,6 @@ export const pokemonFixtures = [
   { name: 'Mareep', nickname: 'RazorMorel', level: 10, place: 'route-201', box: 'team', ability: 'arena-trap', pokeball: 'pokeball' },
   { name: 'Sealeo', nickname: 'Darkdimon', level: 10, place: 'route-203', box: 'team', ability: 'shell-armor', pokeball: 'pokeball' },
   { name: 'Jigglypuff', nickname: 'Vancleemp', level: 8, place: '', box: 'graveyard', pokeball: 'pokeball' },
-  { name: 'Latias', nickname: 'RayThird', level: 5, place: 'oreburgh-gate', box: 'team', ability: 'leaf-guard', pokeball: 'pokeball' }
+  { name: 'Latias', nickname: 'RayThird', level: 5, place: 'oreburgh-gate', box: 'team', ability: 'leaf-guard', pokeball: 'pokeball' },
+  { name: 'Spiritomb', nickname: 'Atreyu', level: 6, place: 'oreburgh-mine', box: 'team', ability: 'anger-point', pokeball: 'pokeball' }
 ]; 
