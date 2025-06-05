7200354d2fe1444509fa85fd4623ecd9c9781f03
Danny Even <danielparejom@gmail.com>
2025-06-05 21:23:54 +0200

    update fixtures

--- src/data/fixtures.ts
+++ src/data/fixtures.ts
@@ -1,9 +1,9 @@
 export const pokemonFixtures = [
-  { name: 'Turtwig', nickname: 'Pablo', level: 10, place: 'route-204', box: 'team', ability: 'keen-eye', pokeball: 'pokeball' },
-  { name: 'Lotad', nickname: 'Calos', level: 10, place: 'verity-lakefront', box: 'team', ability: 'gluttony', pokeball: 'pokeball' },
-  { name: 'Mareep', nickname: 'RazorMorel', level: 10, place: 'route-201', box: 'team', ability: 'arena-trap', pokeball: 'pokeball' },
-  { name: 'Sealeo', nickname: 'Darkdimon', level: 10, place: 'route-203', box: 'team', ability: 'shell-armor', pokeball: 'pokeball' },
+  { name: 'Turtwig', nickname: 'Pablo', level: 14, place: 'route-204', box: 'team', ability: 'keen-eye', pokeball: 'pokeball' },
+  { name: 'Lombre', nickname: 'Calos', level: 14, place: 'verity-lakefront', box: 'team', ability: 'gluttony', pokeball: 'pokeball' },
+  { name: 'Mareep', nickname: 'RazorMorel', level: 14, place: 'route-201', box: 'team', ability: 'arena-trap', pokeball: 'pokeball' },
+  { name: 'Sealeo', nickname: 'Darkdimon', level: 14, place: 'route-203', box: 'team', ability: 'shell-armor', pokeball: 'pokeball' },
   { name: 'Jigglypuff', nickname: 'Vancleemp', level: 8, place: '', box: 'graveyard', pokeball: 'pokeball' },
-  { name: 'Latias', nickname: 'RayThird', level: 5, place: 'oreburgh-gate', box: 'team', ability: 'leaf-guard', pokeball: 'pokeball' },
-  { name: 'Spiritomb', nickname: 'Atreyu', level: 6, place: 'oreburgh-mine', box: 'team', ability: 'anger-point', pokeball: 'pokeball' }
+  { name: 'Latias', nickname: 'RayThird', level: 14, place: 'oreburgh-gate', box: 'team', ability: 'leaf-guard', pokeball: 'pokeball' },
+  { name: 'Spiritomb', nickname: 'Atreyu', level: 14, place: 'oreburgh-mine', box: 'team', ability: 'anger-point', pokeball: 'pokeball' }
 ]; 
